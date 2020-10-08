3dd1a9a098edb64914a94160776a8eebd6b76fec
inq1337 <inquisitor2410@gmail.com>
2020-10-08 23:13:01 +0300

    upstream 7 cpp

--- Practice/07/C++/triangle/triangle/triangle.cpp
+++ Practice/07/C++/triangle/triangle/triangle.cpp
@@ -13,21 +13,17 @@ int main()
 	{
 	case 1:
 		cout << "Введите длины сторон a, b, c\n";
-		cin >> a;
-		cin >> b;
-		cin >> c;
+		cin >> a >> b >> c;
 		if (a > 0 and b > 0 and c > 0)
 		{
-			if (b + c > a and a + c > b and b + a > c)
-			{
-				p = (a + b + c) / 2;
-				S = sqrt(p * (p - a) * (p - b) * (p - c));
-				cout << "Площадь треугольника S = " << S;
-			}
-			else
+			while ((b + c <= a) or (a + c <= b) or (b + a <= c))
 			{
 				cout << "Такого треугольника не существует. Измените параметры\n\n";
+				cin >> a >> b >> c;
 			}
+			p = (a + b + c) / 2;
+			S = sqrt(p * (p - a) * (p - b) * (p - c));
+			cout << "Площадь треугольника S = " << S;
 		}
 		else
 		{
@@ -43,21 +39,27 @@ int main()
 		cin >> xB >> yB;
 		cout << "\nВведите координаты вершины C\n";
 		cin >> xC >> yC;
-		a = sqrt((xC - xB) + (yC - yB));
-		b = sqrt((xA - xC) + (yA - yC));
-		c = sqrt((xB - xA) + (yB - yA));
+		a = sqrt((xC - xB) * (xC - xB) + (yC - yB) * (yC - yB));
+		b = sqrt((xA - xC) * (xA - xC) + (yA - yC) * (yA - yC));
+		c = sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
 		if (a > 0 and b > 0 and c > 0)
 		{
-			if (b + c > a and a + c > b and b + a > c)
-			{
-				p = (a + b + c) / 2;
-				S = sqrt(p * (p - a) * (p - b) * (p - c));
-				cout << "Площадь треугольника S = " << S;
-			}
-			else
+			while ((b + c <= a) or (a + c <= b) or (b + a <= c))
 			{
 				cout << "Такого треугольника не существует. Измените параметры\n\n";
+				cout << "Введите координаты вершины A\n";
+				cin >> xA >> yA;
+				cout << "\nВведите координаты вершины B\n";
+				cin >> xB >> yB;
+				cout << "\nВведите координаты вершины C\n";
+				cin >> xC >> yC;
+				a = sqrt((xC - xB) * (xC - xB) + (yC - yB) * (yC - yB));
+				b = sqrt((xA - xC) * (xA - xC) + (yA - yC) * (yA - yC));
+				c = sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
 			}
+			p = (a + b + c) / 2;
+			S = sqrt(p * (p - a) * (p - b) * (p - c));
+			cout << "Площадь треугольника S = " << S;
 		}
 		else
 		{
@@ -65,7 +67,7 @@ int main()
 			return main();
 		}
 		break;
-	default: 
+	default:
 		cout << "Неверный ввод. Попробуйте ещё раз\n\n";
 		return main();
 	}
