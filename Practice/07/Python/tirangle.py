cec65a6b88e55a2a44854ade4cee6aaf410fa061
inq1337 <inquisitor2410@gmail.com>
2020-10-10 14:24:02 +0300

    finally 7 on python

--- Practice/07/Python/tirangle.py
+++ Practice/07/Python/tirangle.py
@@ -16,9 +16,29 @@ while True:
                 a = float(input())
                 b = float(input())
                 c = float(input())
-                continue
         break
     elif method == 2:
+        print('Введите попарно координаты вершин x и y')
+        xA, yA = map(float,input('Введите координаты вершины A\n').split())
+        xB, yB = map(float,input('Введите координаты вершины B\n').split())
+        xC, yC = map(float,input('Введите координаты вершины C\n').split())
+        a = ((xC - xB) * (xC - xB) + (yC - yB) * (yC - yB))**0.5
+        b = ((xA - xC) * (xA - xC) + (yA - yC) * (yA - yC))**0.5
+        c = ((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA))**0.5
+        while True:    
+            if a>0 and b>0 and c>0 and b+c>a and c+a>b and a+b>c:
+                p = (a + b + c) / 2
+                S = (p * (p - a) * (p - b) * (p - c))**0.5
+                print('Площадь треугольника S = ', S, sep="")
+                break
+            else:
+                print('Такого треугольника не существует. Попробуйте ещё  раз')
+                xA, yA = map(float,input('Введите координаты вершины A\n').split())
+                xB, yB = map(float,input('Введите координаты вершины B\n').split())
+                xC, yC = map(float,input('Введите координаты вершины C\n').split())
+                a = ((xC - xB) * (xC - xB) + (yC - yB) * (yC - yB))**0.5
+                b = ((xA - xC) * (xA - xC) + (yA - yC) * (yA - yC))**0.5
+                c = ((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA))**0.5
         break
     else:
         print('Введите 1 или 2')
