93eeded92943cc44eb7ac0d64b6d4a84f1ab0ec7
inq1337 <inquisitor2410@gmail.com>
2020-11-13 13:39:22 +0300

    23 v2.2 + readme

--- Practice/23/C++/headers/headers.cpp
+++ Practice/23/C++/headers/headers.cpp
@@ -8,10 +8,11 @@ int main()
 	using std::cout;
 	using std::endl;
 
-	cout << "n\t" << "n!\t" <<endl;
+	cout << "n\t" << "n!" <<endl;
 	for (int n = 1; n <= 10; n++)
 		cout << n << "\t" << factorial(n) << endl;
-	cout << endl;
+	cout << endl << "x\t" << "sin(x)" << endl;
 	for (double x = 0; x <= 3.1415 / 4; x = x + 3.1415 / 180)
 		cout << std::setprecision(4) << x << "\t" << sin(x, 5) << endl;
+	cout << endl;
 }
