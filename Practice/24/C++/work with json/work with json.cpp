fcc926e143633e7451628155f59bd456c5290521
inq1337 <inquisitor2410@gmail.com>
2020-11-18 01:47:42 +0300

    get rid of unnecessary array

--- Practice/24/C++/work with json/work with json.cpp
+++ Practice/24/C++/work with json/work with json.cpp
@@ -10,30 +10,29 @@ int main()
 	std::ifstream input("in.json");
 	json indata;
 	input >> indata;
-	int *completed;
-	int uc = 0;
+	int uc = 0, completed;
 	for (int i = 0; i < indata.size(); i++)
 	{
 		if (indata[i]["userId"] >= uc)
 			uc = indata[i]["userId"];
 	}
-	completed = new int[uc] { 0 };
-
-	for (int i = 0; i < indata.size(); i++)
-	{
-		if (indata[i]["completed"] == true)
-			completed[indata[i]["userId"] - 1]++;
-	}
 
 	json outdata = json::array();
 	for (int i = 1; i <= uc; i++)
 	{
-		json object;
-		object["task_completed"] = completed[i - 1];
-		object["userId"] = i;
-		outdata.push_back(object);
+		completed = 0;
+		for (int j = 0; j < indata.size(); j++)
+			if (indata[j]["completed"] == true and indata[j]["userId"] == i)
+				completed++;
+
+		if (completed > 0)
+		{
+			json object;
+			object["task_completed"] = completed;
+			object["userId"] = i;
+			outdata.push_back(object);
+		}
 	}
-	delete[] completed;
 	std::ofstream output("out.json");
 	output << std::setw(2) << outdata << std::endl;
 }
