4065a2955666116b71491a151460343fddfadac8
inq1337 <inquisitor2410@gmail.com>
2020-11-22 18:53:19 +0300

    added raw respinse and some optimizations

--- Lab/01/C++/Server/Server.cpp
+++ Lab/01/C++/Server/Server.cpp
@@ -5,100 +5,24 @@
 #include <fstream>
 
 using json = nlohmann::json;
-using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::ofstream;
 using namespace httplib;
 
-string GetTime();
-json GetWeather();
-
-bool is_empty_file(std::ifstream& pFile)
-{
-    return pFile.peek() == std::ifstream::traits_type::eof();
-}
-
-
-void gen_response(const Request& req, Response& res)
-{   
-    json scache;
-    std::ifstream rc("cache.json");
-    bool not_exist_cache = !rc.is_open() or is_empty_file(rc);
-
-    if (not_exist_cache)
-    {
-        scache = GetWeather();
-        std::ofstream wc("cache.json");
-        cout << "Generating cache..." << endl;
-        wc << std::setw(2) << scache << std::endl;
-    }
-    else
-    {
-        rc >> scache;
-        cout << "Cache succesfully read" << endl;
-    }
-    string temp = scache;
-    json cache = json::parse(temp);
-
-    int curr_hour;
-    long unixtime;
-    bool do_need_update = true;
-
-    json curr_time_full = json::parse(GetTime());
-    unixtime = curr_time_full["unixtime"];
-    for (int i = 0; i < 48; i++)
-    {
-        long w_unixtime = cache["hourly"][i]["dt"];
-        if (unixtime < w_unixtime)
-        {
-            curr_hour = i;
-            do_need_update = false;
-            break;
-        }
-    }
-
-    if (do_need_update)
-    {
-        scache = GetWeather();
-        std::ofstream wc("cache.json");
-        wc << std::setw(2) << scache << std::endl;
-        cout << "Generating cache..." << endl;
-    }
-
-
-    string widget;
-    ifstream rw("template.html");
-    if (rw.is_open())
-    {
-        getline(rw, widget, '\0');
-    }
-    else
-        cout << "Can`t open template";
-
-    string r1 = "{hourly[i].weather[0].description}";
-    string r2 = "{hourly[i].weather[0].icon}";
-    string r3 = "{hourly[i].temp}";
-
-    string r4 = std::to_string(int(std::round(cache["hourly"][curr_hour]["temp"].get<double>())));
-
-    widget.replace(widget.find(r1), r1.length(), cache["hourly"][curr_hour]["weather"][0]["description"]);
-    widget.replace(widget.find(r2), r2.length(), cache["hourly"][curr_hour]["weather"][0]["icon"]);
-    widget.replace(widget.find(r3), r3.length(), r4);
-    widget.replace(widget.find(r3), r3.length(), r4);
-
-	res.set_content(widget, "text/html");
-}
+void gen_response(const Request& req, Response& res);
+void gen_response_raw(const Request& req, Response& res);
 
 int main()
 {
-
-
 	Server weather;
 	weather.Get("/", gen_response);
-	std::cout << "Start server... OK\n";
+    weather.Get("/raw", gen_response_raw);
+	cout << "Start server... OK\n";
 	weather.listen("localhost", 3000);
 }
-
-
-
 
 json GetWeather()
 {
@@ -112,13 +36,13 @@ json GetWeather()
         }
         else
         {
-            std::cout << "Status code: " << res->status << std::endl;
+            cout << "Status code: " << res->status << endl;
         }
     }
     else
     {
         auto err = res.error();
-        std::cout << "Error code: " << err << std::endl;
+        cout << "Error code: " << err << endl;
     }
 }
 
@@ -134,12 +58,134 @@ string GetTime()
         }
         else
         {
-            std::cout << "Status code: " << res->status << std::endl;
+            cout << "Status code: " << res->status << endl;
         }
     }
     else
     {
         auto err = res.error();
-        std::cout << "Error code: " << err << std::endl;
+        cout << "Error code: " << err << endl;
     }
+}
+
+bool is_empty_file(std::ifstream& pFile)
+{
+    return pFile.peek() == ifstream::traits_type::eof();
+}
+
+json CacheGenerator(ifstream& ReadCache)
+{
+    json RawCache;
+    RawCache = GetWeather();
+    ofstream wc("cache.json");
+    cout << "Generating cache..." << endl;
+    wc << std::setw(2) << RawCache << std::endl;
+    return RawCache;
+}
+
+string StringRemoover(string FToRemoove, json cache, int curr_hour)
+{
+    string r1 = "{hourly[i].weather[0].description}";
+    string r2 = "{hourly[i].weather[0].icon}";
+    string r3 = "{hourly[i].temp}";
+
+    double tempd = cache["hourly"][curr_hour]["temp"];
+    string temps = std::to_string(int(round(tempd)));
+    string desk = cache["hourly"][curr_hour]["weather"][0]["description"];
+    string icon = cache["hourly"][curr_hour]["weather"][0]["icon"];
+
+    FToRemoove.replace(FToRemoove.find(r1), r1.length(), desk);
+    FToRemoove.replace(FToRemoove.find(r2), r2.length(), icon);
+    FToRemoove.replace(FToRemoove.find(r3), r3.length(), temps);
+    FToRemoove.replace(FToRemoove.find(r3), r3.length(), temps);
+    return FToRemoove;
+}
+
+int WhatHour(json cache)
+{
+    int curr_hour = 100;
+    long unixtime;
+
+    json curr_time_full = json::parse(GetTime());
+    unixtime = curr_time_full["unixtime"];
+    for (int i = 0; i < 48; i++)
+    {
+        long w_unixtime = cache["hourly"][i]["dt"];
+        if (unixtime < w_unixtime)
+        {
+            curr_hour = i;
+            break;
+        }
+    }
+    return curr_hour;
+}
+
+json CacheReader(ifstream& rc)
+{
+    json RawCache;
+    bool not_exist_cache = !rc.is_open() or is_empty_file(rc);
+
+    if (not_exist_cache)
+    {
+        RawCache = CacheGenerator(rc);
+    }
+    else
+    {
+        rc >> RawCache;
+        cout << "Cache succesfully read" << endl;
+    }
+    return RawCache;
+}
+
+void gen_response_raw(const Request& req, Response& res)
+{
+    ifstream rc("cache.json");
+    json RawCache = CacheReader(rc);
+    string temp = RawCache;
+    json cache = json::parse(temp);
+    int curr_hour = WhatHour(cache);
+
+    if (curr_hour == 100)
+    {
+        RawCache = CacheGenerator(rc);
+        temp = RawCache;
+        cache = json::parse(temp);
+    }
+
+    json WeatherData;
+    double tempd = cache["hourly"][curr_hour]["temp"];
+    int tempi = round(tempd);
+    string desk = cache["hourly"][curr_hour]["weather"][0]["description"];
+
+    WeatherData["temperature"] = tempi;
+    WeatherData["description"] = desk;
+    res.set_content(WeatherData.dump(), "text/json");
+}
+
+void gen_response(const Request& req, Response& res)
+{
+    ifstream rc("cache.json");
+    json RawCache = CacheReader(rc);
+    string temp = RawCache;
+    json cache = json::parse(temp);
+    int curr_hour = WhatHour(cache);
+
+    if (curr_hour == 100)
+    {
+        RawCache = CacheGenerator(rc);
+        temp = RawCache;
+        cache = json::parse(temp);
+    }
+
+    string widget;
+    ifstream rw("template.html");
+    if (rw.is_open())
+    {
+        getline(rw, widget, '\0');
+    }
+    else
+        cout << "Can`t open template";
+
+    string output = StringRemoover(widget, cache, curr_hour);
+    res.set_content(output, "text/html");
 }
