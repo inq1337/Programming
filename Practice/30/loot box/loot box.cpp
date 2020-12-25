0a78cc101914a9e49cecb5d9e585eef0257827ff
inq1337 <inquisitor2410@gmail.com>
2020-12-25 19:56:49 +0300

    started 30 + readme

---
+++ Practice/30/loot box/loot box.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+
+struct Coin {
+	short count;
+};
+
+struct Rune {
+	short level;
+	enum class Elenemt {
+		FIRE,
+		WATER,
+		EARTH,
+		AIR
+	} element;
+};
+
+struct Weapon {
+	short damage, critical, durability;
+};
+
+struct Armor {
+	short guard, durability;
+};
+
+enum class ItemType {
+	COIN,
+	RUNE,
+	WEAPON,
+	ARMOR
+};
+
+struct Item {
+	enum class ItemType {
+		COIN,
+		RUNE,
+		WEAPON,
+		ARMOR
+	} type;
+	union Types {
+		Coin coin;
+		Rune rune;
+		Weapon weapon;
+		Armor armor;
+	};
+};
+
+int main()
+{
+	srand(time(0));
+	int chance = (rand() % 10001) + 1;
+
+	std::vector<Item> LootBox;
+
+}
