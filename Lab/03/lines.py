43726f3256d26edcd1a6a3eb07b2499e5e0b59f5
inq1337 <71176599+inq1337@users.noreply.github.com>
2021-01-14 17:55:46 +0300

    lab 3 v1 + started 32

---
+++ Lab/03/lines.py
@@ -0,0 +1,532 @@
+from tkinter import Tk, Label, Frame, Button
+from PIL import Image, ImageTk
+from os import path
+from random import randint
+
+
+class FieldTile:
+    def __init__(self, name, size):
+        resources_folder = path.realpath(path.dirname(__file__) + '/resources')
+        self._image_path = resources_folder + f'/{name}.png'
+        self.normal
+        self.size = size
+
+        self.taken = [None for _ in range(10)]
+
+    @property
+    def rgba(self):
+        self.image = Image.open(self._image_path).convert('RGBA')
+        return self
+
+    @property
+    def normal(self):
+        self.image = Image.open(self._image_path)
+        return self
+
+    def take(self, i):
+        if not self.taken[i]:
+            taken = self.cut(i)
+            taken_photo = ImageTk.PhotoImage(taken)
+            self.taken[i] = taken_photo
+            return taken_photo
+
+        else:
+            return self.taken[i]
+
+    def cut(self, i):
+        step = self.size
+        xs = 1
+        xe = self.size
+        ys = (i - 1) * step
+        ye = i * step
+        taken = self.image.crop((xs, ys, xe, ye))
+
+        return taken
+
+
+class ViewFieldTiles:
+    def __init__(self):
+        self.ball_aqua = FieldTile('ball-aqua', 60)
+        self.ball_blue = FieldTile('ball-blue', 60)
+        self.ball_green = FieldTile('ball-green', 60)
+        self.ball_pink = FieldTile('ball-pink', 60)
+        self.ball_red = FieldTile('ball-red', 60)
+        self.ball_violet = FieldTile('ball-violet', 60)
+        self.ball_yellow = FieldTile('ball-yellow', 60)
+
+        self.cell = FieldTile('cell-bgr', 69)
+        self.page = FieldTile('page-bgr', 128)
+
+
+tiles = None
+
+
+class Position:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+def differ(pos1, pos2):
+    return Position(pos1.x - pos2.x, pos1.y - pos2.y)
+
+
+class ViewFieldCell(Label):
+    def __init__(self, master, row, col, field):
+        self.ball_tile = None
+        self.backed = None
+        self.field: list[list[ViewFieldCell]] = field
+
+        super().__init__(master, image=tiles.cell.take(1), borderwidth=0)
+        self.grid(row=row, column=col)
+        self.bind('<Button-1>', self.on_click)
+        self.click_handler = None
+        self.pos = Position(col, row)
+        self.is_prev = False
+
+    def put_ball(self, ball_tile: FieldTile):
+        self.to_blend_bg_ball(1, ball_tile, 1)
+
+    def select_ball(self):
+        self.to_blend_bg_ball(2, self.ball_tile, 1)
+
+    def unselect_ball(self):
+        self.to_blend_bg_ball(1, self.ball_tile, 1)
+
+    def to_blend_bg_ball(self, bg_tile_num, ball_tile, ball_tile_num):
+        bg = tiles.cell.rgba.cut(bg_tile_num)
+        ball = ball_tile.rgba.cut(ball_tile_num)
+        ball_new = Image.new('RGBA', bg.size)
+        ball_new.paste(ball, (7, 6))
+        baked = Image.alpha_composite(bg, ball_new)
+        baked_photo = ImageTk.PhotoImage(baked)
+        self.backed = baked_photo
+        self.ball_tile = ball_tile
+        self.config(image=baked_photo)
+
+    def clear(self):
+        self.backed = None
+        self.config(image=tiles.cell.take(1))
+
+    def on_click(self, event):
+        if self.click_handler:
+            self.click_handler(self, event)
+
+    @property
+    def is_free(self):
+        return not self.backed
+
+    def sibling_in(self, direction: Position):
+        rev_dir = Position(-direction.x, -direction.y)
+        new_pos = differ(self.pos, rev_dir)
+        if (new_pos.x < 0 or new_pos.y < 0 or
+                new_pos.y > len(self.field) - 1 or new_pos.x > len(self.field[0]) - 1):
+            return None
+        return self.field[new_pos.y][new_pos.x]
+
+
+class ViewGameInfo:
+    def __init__(self, master):
+        self.lines = Label(master, font=("Google Sans", 20), bg='#414141', fg='#ffffff')
+        self.lines.grid(row=1, column=1, columnspan=3)
+        self.score = Label(master, font=("Google Sans", 20), bg='#414141', fg='#ffffff')
+        self.score.grid(row=2, column=1, columnspan=3)
+        self.update_lines(0)
+        self.update_score(0)
+
+    def update_lines(self, value):
+        self.lines.config(text=f'Линии {value}')
+
+    def update_score(self, value):
+        self.score.config(text=f'Счёт: {value}')
+
+
+class GameControls:
+    def __init__(self, master):
+        Label(master, height=1, background='#414141').grid(row=3, column=1, columnspan=3)
+        self.step = Button(master, text="Сделать ход", font=("Google Sans", 15), bg='#6e7078', fg='#eeeeee', padx=2,
+                           pady=1, relief='flat',
+                           width=15)
+        self.step.grid(row=6, column=1, columnspan=3)
+        self.new_game = Button(master, text="Новая игра", font=("Google Sans", 15), bg='#6e7078', fg='#eeeeee', padx=2,
+                               pady=1, relief='flat',
+                               width=15)
+        self.new_game.grid(row=10, column=1, columnspan=3)
+
+
+class GameHint(list):
+    def __init__(self, master):
+        super().__init__()
+        Label(master, height=2, background='#414141').grid(row=7, column=1, columnspan=3)
+        Label(master, text='Подсказка:', font=("Google Sans", 15), bg='#414141', fg='#eeeeee').grid(row=7, column=1,
+                                                                                                    columnspan=3)
+        self.model = []
+        for i in range(3):
+            self.append(Label(master, bg='#414141'))
+            self.model.append(None)
+            self[i].grid(row=9, column=1 + i)
+
+    def update(self: list, three_balls: list):
+        i = 0
+        for ball in three_balls:
+            self[i].config(image=ball.take(5))
+            self.model[i] = ball
+            i += 1
+
+
+class GameInfo:
+    def __init__(self, master):
+        master.config(bg='#414141')
+        self.controls = GameControls(master)
+        self.hint = GameHint(master)
+        self.info = ViewGameInfo(master)
+
+
+class Interface:
+    def __init__(self, field_size):
+        global tiles
+
+        self.root = Tk()
+        self.root.title('Линии')
+        aside_width = 200
+        width = 68 * field_size + aside_width
+        height = 68 * field_size + 10
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        x_coordinate = int((screen_width / 2) - (width / 2))
+        y_coordinate = int((screen_height / 2) - (height / 2))
+        self.size = Position(width, height)
+        self.root.geometry("{}x{}+{}+{}".format(width, height, x_coordinate, y_coordinate))
+
+        tiles = ViewFieldTiles()
+        self.tiles = tiles
+
+        self.root.config(background='#414141')
+
+        self.field_frame = Frame(self.root)
+        self.field_frame.grid(row=1, column=1)
+
+        self.aside_frame = Frame(self.root, width=aside_width)
+        self.aside_frame.grid(row=1, column=2)
+        self.aside = GameInfo(self.aside_frame)
+
+        self.field = []
+        for row in range(field_size):
+            self.field.append([])
+            for col in range(field_size):
+                cell = ViewFieldCell(self.field_frame, row, col, self.field)
+                self.field[row].append(cell)
+
+        self.game_over = Label(self.field_frame, text="Игра окончена", fg='white', bg='#414141',
+                               font=("Google Sans", 45))
+
+    @property
+    def game_over_visibility(self):
+        pass
+
+    @game_over_visibility.setter
+    def game_over_visibility(self, visible: bool):
+        if visible:
+            self.game_over.place(x=self.size.x / 4.8, y=self.size.y / 2.2)
+        else:
+            self.game_over.place_forget()
+
+    def render(self):
+        self.root.mainloop()
+
+
+class ModelField(list):
+    def __init__(self, field: list):
+        super().__init__()
+        self.field = field
+
+        size = len(field)
+
+        def get_square_dialog(square_shift_x, square_shift_y, square_size, reverse=False):
+            nonlocal field
+
+            dialog = []
+            for j in range(square_size):
+                if not reverse:
+                    zx = j
+                    zy = j
+                else:
+                    zx = square_size - 1 - j
+                    zy = j
+                dialog.append(field[square_shift_y + zy][square_shift_x + zx])
+
+            return dialog
+
+        rows = field
+
+        cols = [[] for cell in field[0]]
+        for row in field:
+            i = 0
+            for cell in row:
+                cols[i].append(cell)
+                i += 1
+
+        main_dialogs = [get_square_dialog(0, 0, size)]
+        for i in range(5, size):
+            k = size - i
+            main_dialogs.append(get_square_dialog(k, 0, i))
+            main_dialogs.append(get_square_dialog(0, k, i))
+
+        sub_dialogs = [get_square_dialog(0, 0, size, True)]
+        for i in range(5, size):
+            k = size - i
+            sub_dialogs.append(get_square_dialog(k, k, i, True))
+            sub_dialogs.append(get_square_dialog(0, 0, i, True))
+
+        self.rows = rows
+        self.cols = cols
+        self.main_dialogs = main_dialogs
+        self.sub_dialogs = sub_dialogs
+
+
+class Model:
+    def __init__(self, view_field: Interface):
+        self.view = view_field
+        self.state = 'Шарик не выбран'
+        self.selected = None
+        self.score = 0
+        self.lines = 0
+        self.field = ModelField(view_field.field)
+
+        self.restart()
+
+        view_field.aside.controls.new_game.bind('<Button-1>', self.restart)
+        view_field.aside.controls.step.bind('<Button-1>', self.step)
+
+        for row in self.view.field:
+            for cell in row:
+                cell.click_handler = self._cell_click_handler
+
+    def restart(self, event=None):
+        self.view.game_over_visibility = False
+        self._clear_field()
+        self.gen_hint()
+        self.step()
+        self.view.aside.info.update_score(0)
+        self.view.aside.info.update_lines(0)
+        self.state = 'шарик не выбран'
+
+    def step(self, event=None):
+        if self.state == 'игра окончена':
+            return
+        self.use_hint()
+        self.gen_hint()
+
+    def gen_hint(self):
+        balls = []
+        for i in range(3):
+            balls.append(self._random_ball())
+        self.view.aside.hint.update(balls)
+
+    def use_hint(self):
+        hint = self.view.aside.hint
+
+        free = self._field_free_cells()
+
+        for i in range(2, -1, -1):
+            if len(free) - i <= 1:
+                self.game_over()
+                return
+            else:
+                cell = self._random_from(free)
+                cell.put_ball(hint.model[i])
+                free.pop()
+
+    def game_over(self):
+
+        self.view.game_over_visibility = True
+
+    @staticmethod
+    def _random_from(list_of):
+        return list_of[randint(0, len(list_of) - 1)]
+
+    def _random_ball(self):
+        field_tiles = self.view.tiles
+        balls = [
+            field_tiles.ball_aqua,
+            field_tiles.ball_blue,
+            field_tiles.ball_green,
+            field_tiles.ball_pink,
+            field_tiles.ball_red,
+            field_tiles.ball_violet,
+            field_tiles.ball_yellow
+        ]
+
+        return self._random_from(balls)
+
+    def _field_free_cells(self):
+        free = []
+        field = self.view.field
+        for row in field:
+            for cell in row:
+                if cell.is_free:
+                    free.append(cell)
+        return free
+
+    def _clear_field(self):
+        field = self.view.field
+
+        for row in field:
+            for cell in row:
+                cell.clear()
+
+    def _cell_click_handler(self, cell: ViewFieldCell, event):
+
+        if cell.is_free:
+            if self.state == 'шарик не выбран':
+                pass
+            elif self.state == 'шарик выбран':
+                reachable = self._cells_reachable(self.selected, cell)
+
+                if reachable:
+                    cell.put_ball(self.selected.ball_tile)
+
+                    self.selected.unselect_ball()
+                    self.selected.clear()
+                    self.selected = None
+
+                    self.state = 'шарик перемещён'
+
+                    lines = self._check_all_lines()
+
+                    for line in lines:
+                        self.lines += 1
+                        self.view.aside.info.update_lines(self.lines)
+
+                        for cell in line:
+                            cell.unselect_ball()
+                            cell.clear()
+
+                            self.score += 2
+                            self.view.aside.info.update_score(self.score)
+
+                    self.state = 'шарик не выбран'
+
+                    if len(lines) == 0:
+                        self.step()
+
+                else:
+                    pass
+        else:
+            if self.state == 'шарик не выбран':
+                self.state = 'шарик выбран'
+                self.selected = cell
+                cell.select_ball()
+            elif self.state == 'шарик выбран':
+                self.selected.unselect_ball()
+                self.selected = cell
+                cell.select_ball()
+
+    def _cells_reachable(self, a: ViewFieldCell, b: ViewFieldCell, print_check_way=False):
+
+        def reset_cells_prev():
+            field = self.view.field
+            for row in field:
+                for cell in row:
+                    cell.is_prev = False
+
+        def step_all_directions(from_cell: ViewFieldCell, target: ViewFieldCell):
+            player_steps = []
+
+            left = Position(1, 0)
+            right = Position(-1, 0)
+            up = Position(0, 1)
+            down = Position(0, -1)
+            for direction in [up, down, left, right]:
+                sibling = from_cell.sibling_in(direction)
+
+                if not sibling:
+                    continue
+
+                if sibling == target:
+                    return True
+                else:
+                    if sibling.is_free and not sibling.is_prev:
+                        sibling.is_prev = True
+                        if print_check_way:
+                            sibling.to_blend_bg_ball(1, self.view.tiles.ball_yellow, 6)
+                        player_steps.append(sibling)
+
+                    elif sibling.is_free and sibling.is_prev:
+                        if print_check_way:
+                            sibling.to_blend_bg_ball(1, self.view.tiles.ball_green, 6)
+
+            return player_steps
+
+        steps = [a]
+
+        while True:
+            new_steps_storage = []
+
+            for step in steps:
+                new_steps = step_all_directions(step, b)
+
+                if new_steps is True:
+                    reset_cells_prev()
+                    return True
+                else:
+                    new_steps_storage += new_steps
+
+            if len(new_steps_storage) == 0:
+                reset_cells_prev
+                return False
+            steps = new_steps_storage
+
+    def _check_all_lines(self):
+
+        def check_lines(field_lines):
+            sequences = []
+
+            for line in field_lines:
+                sequence = []
+                prev_color = None
+
+                def reset_sequence(color, field_cell=None):
+                    nonlocal sequence, prev_color
+
+                    if len(sequence) > 4:
+                        sequences.append(sequence)
+
+                    prev_color = color
+                    sequence = [field_cell] if field_cell else []
+
+                for cell in line:
+                    if cell.is_free:
+                        reset_sequence(None)
+                        continue
+
+                    else:
+                        curr_color = cell.ball_tile
+
+                        if prev_color is None:
+                            reset_sequence(curr_color, cell)
+
+                        elif curr_color == prev_color:
+                            sequence.append(cell)
+
+                        else:
+                            reset_sequence(curr_color, cell)
+
+                reset_sequence(None)
+
+            return sequences
+
+        lines = []
+        lines += check_lines(self.field.rows)
+        lines += check_lines(self.field.cols)
+        lines += check_lines(self.field.main_dialogs)
+        lines += check_lines(self.field.sub_dialogs)
+
+        return lines
+
+
+ui = Interface(10)
+gameModel = Model(ui)
+
+ui.render()
---
+++ Practice/32/C++/titanic/titanic.cpp
@@ -0,0 +1,77 @@
+﻿#pragma warning(disable:4996) //to disable secure warnings, wich cased by csv.hpp
+#include <iostream>
+#include <tuple>
+#include <csv.hpp> //https://github.com/vincentlaucsb/csv-parser
+
+using std::string;
+using std::vector;
+
+struct Passenger {
+	bool survival;
+	short pclass;
+	string name;
+	string sex;
+	float age;
+	short sibsp;
+	short parch;
+	string ticket;
+	float fare;
+	string cabin;
+	char embarked;
+};
+
+std::istream& operator>> (std::istream& in, vector<Passenger>& passengers) 
+{
+	csv::CSVReader reader("train.csv");
+
+	for (auto& row : reader) 
+	{
+		Passenger passenger {
+			row["Survived"].get<bool>(),
+			row["Pclass"].get<short>(),
+			row["Name"].get(),
+			row["Sex"].get(),
+			row["Age"].get<float>(),
+			row["SibSp"].get<short>(),
+			row["Parch"].get<short>(),
+			row["Ticket"].get(),
+			row["Fare"].get<float>(),
+			row["Cabin"].get(),
+			row["Embarked"].get<char>()
+		};
+		passengers.push_back(passenger);
+	}
+
+	return in;
+}
+
+std::ostream& operator << (std::ostream& out, vector<Passenger>& passengers)
+{
+	auto writer = csv::make_csv_writer(out);
+
+	writer << vector<string>({"Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"});
+
+	for (auto& pass : passengers) 
+	{
+		writer << std::make_tuple(
+			pass.survival,
+			pass.pclass,
+			pass.name,
+			pass.sex,
+			pass.age,
+			pass.sibsp,
+			pass.parch,
+			pass.ticket,
+			pass.fare,
+			pass.cabin,
+			pass.embarked
+		);
+	}
+
+	return out;
+}
+
+int main()
+{
+
+}
