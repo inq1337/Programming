5583eae1092454b381852d2019bfe92246ad6863
inq1337 <71176599+inq1337@users.noreply.github.com>
2021-01-17 21:15:10 +0300

    31 comments and 32

--- Practice/32/C++/titanic/titanic.cpp
+++ Practice/32/C++/titanic/titanic.cpp
@@ -1,12 +1,12 @@
-﻿#pragma warning(disable:4996) //to disable secure warnings, wich cased by csv.hpp
-#include <iostream>
-#include <tuple>
-#include <csv.hpp> //https://github.com/vincentlaucsb/csv-parser
+﻿#include <fstream>
+#include <vector>
+#include <string>
 
 using std::string;
 using std::vector;
 
 struct Passenger {
+	short pass_id;
 	bool survival;
 	short pclass;
 	string name;
@@ -20,58 +20,172 @@ struct Passenger {
 	char embarked;
 };
 
-std::istream& operator>> (std::istream& in, vector<Passenger>& passengers) 
+void replace_all(string& str, const string& to_replace, const string& replace_with)
 {
-	csv::CSVReader reader("train.csv");
-
-	for (auto& row : reader) 
+	size_t pos = str.find(to_replace);
+	while (pos != string::npos)
 	{
-		Passenger passenger {
-			row["Survived"].get<bool>(),
-			row["Pclass"].get<short>(),
-			row["Name"].get(),
-			row["Sex"].get(),
-			row["Age"].get<float>(),
-			row["SibSp"].get<short>(),
-			row["Parch"].get<short>(),
-			row["Ticket"].get(),
-			row["Fare"].get<float>(),
-			row["Cabin"].get(),
-			row["Embarked"].get<char>()
-		};
-		passengers.push_back(passenger);
+		str.replace(pos, to_replace.size(), replace_with);
+		pos = str.find(to_replace, pos + replace_with.size());
+	}
+}
+
+vector<vector<string>> parseCSV(std::istream& out)
+{
+	string ln;
+	string buff;
+
+	vector<string> sep_str;
+	vector<vector<string>> res;
+
+	char quote = 0;
+	char pos = 0;
+
+	while (getline(out, ln, '\r'))
+	{
+		pos = 0;
+		quote = 0;
+
+		for (char ch : ln) 
+		{
+			if (ch == ',')
+			{
+				switch (quote) 
+				{
+				case 0:
+					sep_str.push_back(buff);
+					buff.clear();
+					++pos;
+					continue;
+				case 2:
+					sep_str.push_back(buff);
+					buff.clear();
+					quote = 0;
+					++pos;
+					continue;
+				}
+			}
+			if (ch == '"')
+			{
+				switch (quote) 
+				{
+				case 0:
+					quote = 1;
+					continue;
+				case 1:
+					quote = 2;
+					continue;
+				case 2:
+					buff.push_back('"');
+					quote = 1;
+					continue;
+				}
+			}
+			buff.push_back(ch);
+		}
+		sep_str.push_back(buff);
+		buff.clear();
+
+		res.push_back(sep_str);
+		sep_str.clear();
+	}
+	return res;
+}
+
+std::istream& operator>> (std::istream& in, std::vector<Passenger>& vector) {
+	auto matrix = parseCSV(in);
+	matrix.erase(matrix.begin());
+
+	for (auto row : matrix)
+	{
+		Passenger pass;
+
+		pass.pass_id = std::stoi(row[0]);
+		pass.survival = std::stoi(row[1]);
+		pass.pclass = std::stoi(row[2]);
+		pass.name = row[3];
+		pass.sex = row[4];
+		pass.age = row[5] == "" ? 0 : std::stof(row[5]);
+		pass.sibsp = std::stoi(row[6]);
+		pass.parch = std::stoi(row[7]);
+		pass.ticket = row[8];
+		pass.fare = std::stof(row[9]);
+		pass.cabin = row[10];
+		pass.embarked = row[11][0];
+
+		vector.push_back(pass);
 	}
 
 	return in;
 }
 
-std::ostream& operator << (std::ostream& out, vector<Passenger>& passengers)
+std::ostream& operator<< (std::ostream& out, vector<Passenger> vector) 
 {
-	auto writer = csv::make_csv_writer(out);
+	out << "PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked" << '\r';
 
-	writer << vector<string>({"Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"});
-
-	for (auto& pass : passengers) 
+	for (auto& pass : vector) 
 	{
-		writer << std::make_tuple(
-			pass.survival,
-			pass.pclass,
-			pass.name,
-			pass.sex,
-			pass.age,
-			pass.sibsp,
-			pass.parch,
-			pass.ticket,
-			pass.fare,
-			pass.cabin,
-			pass.embarked
-		);
-	}
+		string name = pass.name;
+		replace_all(name, "\"", "\"\"");
 
+		out << pass.pass_id << ','
+			<< pass.survival << ','
+			<< short(pass.pclass) << ','
+			<< '"' << name << '"' << ','
+			<< pass.sex << ','
+			<< short(pass.age) << ','
+			<< short(pass.sibsp) << ','
+			<< short(pass.parch) << ','
+			<< pass.ticket << ','
+			<< pass.fare << ','
+			<< pass.cabin << ','
+			<< pass.embarked << '\r';
+	}
 	return out;
+}
+
+void bubbleSort(std::vector<Passenger*>& vector) 
+{
+	int size = vector.size();
+	for (int i = 0; i < size - 1; ++i) 
+	{
+		for (int j = size - 1; j > i; --j)
+		{
+			if (vector[j]->age < vector[j - 1]->age)
+				std::swap(*vector[j], *vector[j - 1]);
+		}
+	}
 }
 
 int main()
 {
+	std::ifstream in("train.csv");
+	vector<Passenger> passengers;
+	vector<Passenger> pass_output;
+	vector<Passenger> females_sorted;
+	vector<Passenger*> females_sorted_ptr;
 
+	in >> passengers;
+
+	for (auto& item : passengers)
+	{
+		if (item.pclass == 1 and item.sex =="female" and item.survival)
+			females_sorted_ptr.push_back(&item);
+	}
+
+	bubbleSort(females_sorted_ptr);
+
+	for (auto& item : females_sorted_ptr)
+	{
+		pass_output.push_back(*item);
+	}
+
+	for (auto& item : passengers)
+	{
+		if (item.pclass != 1 and !(item.sex == "female" and item.survival))
+			pass_output.push_back(item);
+	}
+
+	std::ofstream out("out.csv");
+	out << pass_output;
 }
