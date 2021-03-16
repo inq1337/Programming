f9759b1af7c6c17fc14a4593be6282c6742791a4
inq1337 <71176599+inq1337@users.noreply.github.com>
2021-03-16 13:54:27 +0300

    fixed 35

--- Practice/35/C++/Integers_array/main.cpp
+++ Practice/35/C++/Integers_array/main.cpp
@@ -11,7 +11,8 @@ void create(IntArray& arr, int size) {
 }
 
 void create(IntArray* arr, int size) {
-    create(*arr, size);
+    arr->data = new int[size];
+    arr->size = size;
 }
 
 int get(IntArray& arr, int index) {
@@ -23,7 +24,11 @@ int get(IntArray& arr, int index) {
 }
 
 int get(IntArray* arr, int index) {
-    return get(*arr, index);
+    if (index < 0 or index >= arr->size) {
+        std::cerr << "The index is outside the array" << std::endl;
+        exit(1);
+    }
+    return arr->data[index];
 }
 
 void set(IntArray& arr, int index, int value) {
@@ -35,7 +40,11 @@ void set(IntArray& arr, int index, int value) {
 }
 
 void set(IntArray* arr, int index, int value) {
-    set(*arr, index, value);
+    if (index < 0 or index >= arr->size) {
+        std::cerr << "The index is outside the array" << std::endl;
+        exit(1);
+    }
+    arr->data[index] = value;
 }
 
 void print(IntArray& arr)
@@ -48,8 +57,14 @@ void print(IntArray& arr)
     std::cout << arr.data[arr.size - 1] << "]";
 }
 
-void print(IntArray* arr) {
-    print(*arr);
+void print(IntArray* arr)
+{
+    std::cout << "[";
+    for (int i = 0; i < arr->size - 1; i++)
+    {
+        std::cout << arr->data[i] << ", ";
+    }
+    std::cout << arr->data[arr->size - 1] << "]";
 }
 
 void resize(IntArray& arr, int newSize) {
@@ -81,7 +96,31 @@ void resize(IntArray& arr, int newSize) {
 }
 
 void resize(IntArray* arr, int newSize) {
-    resize(*arr, newSize);
+    if (newSize > arr->size) {
+        int* newArr = new int[newSize];
+
+        for (int i = 0; i < arr->size; ++i) {
+            newArr[i] = arr->data[i];
+        }
+        for (int i = arr->size; i < newSize; ++i) {
+            newArr[i] = 0;
+        }
+
+        delete[] arr->data;
+        arr->data = newArr;
+        arr->size = newSize;
+    }
+    else {
+        int* newArr = new int[newSize];
+
+        for (int i = 0; i < newSize; ++i) {
+            newArr[i] = arr->data[i];
+        }
+
+        delete[] arr->data;
+        arr->data = newArr;
+        arr->size = newSize;
+    }
 }
 
 void destroy(IntArray& arr) {
@@ -95,7 +134,13 @@ void destroy(IntArray& arr) {
 }
 
 void destroy(IntArray* arr) {
-    destroy(*arr);
+    if (!arr->data) {
+        return;
+    }
+
+    delete[] arr->data;
+    arr->data = nullptr;
+    arr->size = 0;
 }
 
 int main() {
